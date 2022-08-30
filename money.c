0e16f0198dcec72ce76e1045a9a8ded54162f967
soumikbhuyan9 <krishbhuyan9@gmail.com>
2022-08-30 11:31:14 +0530

    ifelse program

---
+++ money.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter your earning\n");
+    scanf("%d", &a);
+    if(a>=500)
+    printf("Keep Earning");
+    else
+    printf("Work Hard");
+    return 0;
+}
