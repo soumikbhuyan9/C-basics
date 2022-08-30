d27571d936338a08644d7ed91b3b3acdbb4cd82f
soumikbhuyan9 <krishbhuyan9@gmail.com>
2022-08-30 12:19:12 +0530

    ascii program

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("input any chr to get ASCII");
+    scanf ("%c", &ch);
+    printf("ASCII value of %c is %d, ch, ch");
+    return 0;
+    }
